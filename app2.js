f5ec7a295c9bf5ea7cf71f5e8ad4c0b38b5e4b6c
Abdur <122381082+codelessway@users.noreply.github.com>
2023-03-31 00:11:58 +0500

    added to, from etc from zapier post request

--- app2.js
+++ app2.js
@@ -1,18 +1,35 @@
 const { SolapiMessageService } = require('solapi');
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json());
 
 app.get('/data', (req, res) => {
     console.log("inside /data route");
   res.send('Hello World!')
 })
 
-app.post('/payload', (req, res) => {
-  console.log(req.body);
-  var body =req.body;
-  res.send(body);
-  //res.json(body);
+ app.post('/sendKakaoFriendTalk', async (req, res) => {
+  const { to, from, pfId, templateId } = req.body;
+  //console.log(to +" "+from+" "+pfId +" "+templateId);
+  // "to": "010-3381-3069",
+  // "from": "010-9571-6736",
+  // "kakaoOptions": {
+  //   "pfId": "KA01PF2303150547448974jVhP14CJV5",
+  //   "templateId": "KA01TP230315073806460ao9PScCLNWu",
+
+  // const to1="010-3381-3069";
+  // const from1 = "010-9571-6736";
+  // const pfId1 ="KA01PF2303150547448974jVhP14CJV5";
+  // const templateId1 = "KA01TP230315073806460ao9PScCLNWu";
+
+
+  let  response =  await sendMessage(to,from,pfId,templateId);
+
+  res.json(response);
 });
 
 app.listen(port, () => {
@@ -21,17 +38,17 @@ app.listen(port, () => {
 
 
 
-function sendMessage(to,from,pfid,templateId){
+async function sendMessage(to,from,pfId,templateId){
 
   const messageService = new SolapiMessageService("NCSYQZXVPTEA5W5T", "TQRTJ0PHF62FNFYHFFTAEADA6ME5TFWI");
 
 // 반드시 발신번호와 수신번호는 01012345678 형식으로 입력해주세요!
-messageService.send({
-  "to": "010-3381-3069",
-  "from": "010-9571-6736",
+return await messageService.send({
+  "to": to,
+  "from": from,
   "kakaoOptions": {
-    "pfId": "KA01PF2303150547448974jVhP14CJV5",
-    "templateId": "KA01TP230315073806460ao9PScCLNWu",
+    "pfId": pfId,
+    "templateId": templateId,
     // 치환문구가 없을 때의 기본 형태
     "variables": {}
 
@@ -45,7 +62,7 @@ messageService.send({
     // disbaleSms 값을 true로 줄 경우 문자로의 대체발송이 비활성화 됩니다.
     // disableSms: true,
   }
-}).then((res)=>{
-return res;
-});
+})
+
+
 }
