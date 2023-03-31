763de04931d48ef4c0fddb8e6cf06f1054ade379
Abdur <122381082+codelessway@users.noreply.github.com>
2023-03-31 12:16:09 +0500

    added screenshot endpoint

--- app2.js
+++ app2.js
@@ -2,10 +2,18 @@ const { SolapiMessageService } = require('solapi');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+//for ss
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+
 const app = express()
 const port = 3000
 
 app.use(bodyParser.json());
+
+// Serve the contents of the "images" directory as static files
+app.use('/images', express.static('images'));
 
 app.get('/data', (req, res) => {
     console.log("inside /data route");
@@ -32,6 +40,20 @@ app.get('/data', (req, res) => {
   res.json(response);
 });
 
+
+// take SS
+app.post('/takeSS', async (req, res) => {
+  console.log("route takess called");
+
+  await deleteFiles();
+  
+  const { url } = req.body;
+  let ss =await takeScreenshot(url,getBaseUrl(req));
+  console.log(ss);
+  res.json({"url":ss});
+});
+
+//start server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
@@ -66,3 +88,127 @@ return await messageService.send({
 
 
 }
+
+
+/*
+const puppeteer = require('puppeteer');
+
+async function takeScreenshot(url) {
+  // Launch a new browser instance
+  const browser = await puppeteer.launch();
+
+  // Open a new page in the browser
+  const page = await browser.newPage();
+
+  // Navigate to the URL
+  await page.goto(url);
+
+  // Take a screenshot of the page
+  const screenshot = await page.screenshot({fullPage: true});
+
+  // Close the browser
+  await browser.close();
+
+  // Return the URL of the screenshot as a data URL
+  return `data:image/png;base64,${screenshot.toString('base64')}`;
+}
+
+// Example usage
+takeScreenshot('https://www.google.com').then((url) => {
+  console.log(url);
+});
+*/
+
+
+
+async function takeScreenshot(url, baseUrl) {
+  // Launch headless Chromium browser
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+
+    // Set viewport and window size
+    await page.setViewport({
+      width: 1080,
+      height: 1080
+    });
+  // Navigate to URL and wait for page to load
+  await page.goto(url, {waitUntil: 'networkidle2'});
+
+  // Capture screenshot of page
+ // const screenshot = await page.screenshot({fullPage: true});
+
+  
+  // Take screenshot
+  const screenshot= await page.screenshot({
+    path: 'screenshot.png',
+    clip: {
+      x: 0,
+      y: 0,
+      width: 1080,
+      height: 1080
+    }
+  });
+
+  // Close browser
+  await browser.close();
+
+  // Generate filename for screenshot image
+  const filename = Date.now() + '.png';
+
+  // Save screenshot image to /images directory
+  const imagePath = path.join(__dirname, 'images', filename);
+  fs.writeFileSync(imagePath, screenshot);
+
+  // Return public URL to screenshot image
+  const publicUrl = `${baseUrl}/images/${filename}`;
+  console.log(publicUrl);
+  return publicUrl;
+}
+
+
+// // Example usage
+// takeScreenshot('https://www.facebook.com','your base url')
+//   .then(publicUrl => {
+//     console.log('Screenshot saved to:', publicUrl);
+//   })
+//   .catch(error => {
+//     console.error('Error taking screenshot:', error);
+//   });
+
+
+  app.get('/', (req, res) => {
+    const baseUrl = getBaseUrl(req);
+    res.send(`Base URL: ${baseUrl}`);
+  });
+
+  function getBaseUrl(req) {
+    const protocol = req.protocol;
+    const host = req.get('host');
+    return `${protocol}://${host}`;
+  }
+
+  //
+  async function deleteFiles(){
+    const directory = './images';
+
+    // Read directory
+    fs.readdir(directory, (err, files) => {
+      if (err) throw err;
+      
+      // Sort files by modification time (oldest to newest)
+      files.sort((a, b) => {
+        return fs.statSync(directory + '/' + a).mtime.getTime() - fs.statSync(directory + '/' + b).mtime.getTime();
+      });
+      
+      // Delete all but the last 5 files
+      const filesToDelete = files.slice(0, -5);
+      filesToDelete.forEach((file) => {
+        fs.unlink(directory + '/' + file, (err) => {
+          if (err) throw err;
+          console.log('Deleted file:', file);
+        });
+      });
+    });
+
+  }
